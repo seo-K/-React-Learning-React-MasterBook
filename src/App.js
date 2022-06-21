286314032f0301743cbb9a1aca9851eaf82fb406
seo-K <dkasid@naver.com>
2022-06-22 00:35:41 +0900

    폴더 정리 및 라우터 추가

--- src/App.js
+++ src/App.js
@@ -2,53 +2,72 @@ import React, { useState } from "react";
 import { Routes, Route } from "react-router-dom";
 import "./App.css";
 
-import Main from "./Main";
-import MyComponent from "./MyComponent";
-import Counter from "./Counter";
-import Say from "./Say";
-import EventPractice from "./EventPractice";
-import EventPracticeFunction from "./EventPracticeFunction";
-import ValidationSample from "./ValidationSample";
-import ScrollBox from "./ScrollBox";
-import LifeCycleSample from "./LifeCycleSample";
-import ErrorBoundary from "./ErrorBoundary";
-console.log("나엄ㅁ?");
+import Header from "./Header";
+import MyComponent from "./props/MyComponent";
+import Counter from "./state/Counter";
+import Say from "./state/Say";
+import EventPractice from "./event/EventPractice";
+import EventPracticeFunction from "./event/EventPracticeFunction";
+import ValidationSample from "./ref/ValidationSample";
+import ScrollBox from "./ref/ScrollBox";
+import IterationSample from "./map/IterationSample";
+import LifeCycleSample from "./livecycle/LifeCycleSample";
+import ErrorBoundary from "./livecycle/ErrorBoundary";
 
 export default function App() {
+  // REF
+
+  // LIFECYCLE
   // 랜덤 색상을 생성합니다.
   function getRandomColor() {
     return "#" + Math.floor(Math.random() * 16777215).toString(16);
   }
-
   const [color, setColor] = useState("#000000");
-
   const handleClick = () => {
     setColor({ color: getRandomColor() });
   };
+
+  // state = {
+  //   color: "#000000",
+  // };
+
+  // handleClick = () => {
+  //   this.setState({
+  //     color: getRandomColor(),
+  //   });
+  // };
+
+  // <div>
+  //       <button onClick={this.handleClick}>랜덤 색상</button>
+  //       <ErrorBoundary>
+  //         <LifeCycleSample color={this.state.color} />
+  //       </ErrorBoundary>
+  //     </div>
   return (
     <Routes>
-      <Route path="/" element={<Main />}>
+      <Route path="/" element={<Header />}>
         <Route
           path="/props"
           element={<MyComponent favoriteNumber={1}>리액트</MyComponent>}
         />
         <Route path="/state" element={<Counter>리액트</Counter>} />
-        <Route path="/state/2" element={<Say />} />
+        <Route path="/state-2" element={<Say />} />
         <Route path="/event" element={<EventPractice />} />
-        <Route path="/event/function" element={<EventPracticeFunction />} />
+        <Route path="/event-function" element={<EventPracticeFunction />} />
         <Route path="/ref" element={<ValidationSample />} />
-        <Route path="/ref/scroll" element={<ScrollBox />} />
-        {/* <Route
-          path="/ref/scroll"
+        <Route path="/ref-scroll" element={<ScrollBox />} />
+        <Route path="/map" element={<IterationSample />} />
+        <Route
+          path="/lifecycle"
           element={
             <div>
-              <button onClick={this.handleClick}>랜덤 색상</button>
+              <button onClick={handleClick}>랜덤 색상</button>
               <ErrorBoundary>
                 <LifeCycleSample color={color} />
               </ErrorBoundary>
             </div>
           }
-        /> */}
+        />
       </Route>
     </Routes>
   );
---
+++ src/Header.js
@@ -0,0 +1,68 @@
+import React, { Component, Fragment } from "react";
+import { NavLink, Outlet } from "react-router-dom";
+
+export default function Header() {
+  return (
+    <Fragment>
+      <header>
+        <NavLink
+          className={({ isActive }) => (isActive ? "selected" : "")}
+          to="/props"
+        >
+          Props
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "selected" : "")}
+          to="/state"
+        >
+          State
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "selected" : "")}
+          to="/state-2"
+        >
+          State2
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "selected" : "")}
+          to="/event"
+        >
+          Event
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "selected" : "")}
+          to="/event-function"
+        >
+          Event2
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "selected" : "")}
+          to="/ref"
+        >
+          Ref
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "selected" : "")}
+          to="/ref-scroll"
+        >
+          RefScroll
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "selected" : "")}
+          to="/map"
+        >
+          Map
+        </NavLink>
+        <NavLink
+          className={({ isActive }) => (isActive ? "selected" : "")}
+          to="/lifecycle"
+        >
+          LifeCycle
+        </NavLink>
+      </header>
+      <main>
+        <Outlet />
+      </main>
+    </Fragment>
+  );
+}
