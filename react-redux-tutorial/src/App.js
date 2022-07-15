0c737de13b2a108a353efd0bd0bfaafcbb60cf90
seo-K <dkasid@naver.com>
2022-07-16 02:16:44 +0900

    feat: Add todoList styling

--- react-redux-tutorial/src/App.js
+++ react-redux-tutorial/src/App.js
@@ -1,15 +1,29 @@
 import './App.css';
+import styled from 'styled-components';
+
 import Counter from './components/Counter';
 import Todos from './components/Todos';
 
 function App() {
     return (
-        <div>
-            <Counter number={0} />
-            <hr />
-            <Todos />
-        </div>
+        <ContentWrap>
+            <div className="inner-wrap">
+                <Counter number={0} />
+                <hr />
+                <Todos />
+            </div>
+        </ContentWrap>
     );
 }
 
 export default App;
+
+const ContentWrap = styled.main`
+    display: flex;
+    justify-content: center;
+    align-items: center;
+
+    & .inner-wrap {
+        width: 400px;
+    }
+`;
--- react-redux-tutorial/src/components/Todos.js
+++ react-redux-tutorial/src/components/Todos.js
@@ -1,12 +1,37 @@
 import React from 'react';
+import styled from 'styled-components';
+import { useState } from 'react';
+import { FiCheckSquare, FiSquare } from 'react-icons/fi';
+import { RiDeleteBinFill } from 'react-icons/ri';
 
 const TodoItems = ({ todo, onToggle, onRemove }) => {
+    const [isChecked, setIsChecked] = useState(false);
+
+    const handleChange = (event) => {
+        if (event.target.checked) {
+            console.log('✅ Checkbox is checked');
+        } else {
+            console.log('⛔️ Checkbox is NOT checked');
+        }
+        setIsChecked((current) => !current);
+    };
+
     return (
-        <div>
-            <input type="checkbox" />
-            <span>예제 텍스트</span>
-            <button>삭제</button>
-        </div>
+        <Items>
+            <div className="checkbox_wrap">
+                <input
+                    id="ListCheck"
+                    type="checkbox"
+                    onChange={handleChange}
+                    value={isChecked}
+                />
+                {isChecked ? <FiCheckSquare /> : <FiSquare />}
+                <label htmlFor="ListCheck">예제 텍스트</label>
+            </div>
+            <button className="del_btn">
+                <RiDeleteBinFill />
+            </button>
+        </Items>
     );
 };
 const Todos = ({
@@ -40,3 +65,43 @@ const Todos = ({
     );
 };
 export default Todos;
+
+const Items = styled.li`
+    display: flex;
+    align-items: center;
+    font-size: 1rem;
+
+    & input {
+        display: none;
+    }
+
+    & label + svg {
+        width: 15px;
+        height: 15px;
+
+        margin-right: 8px;
+    }
+
+    & label {
+        cursor: pointer;
+    }
+
+    & .del_btn {
+        width: 20px;
+        height: 20px;
+        display: flex;
+        align-items: center;
+
+        margin-left: auto;
+
+        & svg {
+            fill: #888;
+            width: 100%;
+            height: 100%;
+        }
+    }
+
+    & + li {
+        margin-top: 10px;
+    }
+`;
